4bcb6dcf2c18d3b4235e1c1cef13634320038546
luca.canali <canali.luca998@gmail.com>
2023-02-08 14:39:08 +0100

    Update javadoc

--- parent/applicazioneCittadini/src/main/java/com/example/applicazionecittadini/GUI/ControllerAccessoCittadino.java
+++ parent/applicazioneCittadini/src/main/java/com/example/applicazionecittadini/GUI/ControllerAccessoCittadino.java
@@ -7,6 +7,9 @@ import javafx.scene.control.Button;
 
 import java.io.IOException;
 
+/**
+ *Classe che gestisce l'interfaccia grafica per l'accesso e la registrazione dei cittadini.
+ */
 public class ControllerAccessoCittadino {
 
     @FXML
@@ -16,11 +19,24 @@ public class ControllerAccessoCittadino {
     @FXML
     private Button btAccessoCittadino;
 
+    /**
+     * Questo metodo gestisce l'evento di pressione del pulsante per la registrazione di un nuovo cittadino.
+     * Chiude la finestra corrente e apre la finestra per la registrazione.
+     * @param event evento di pressione del pulsante
+     * @throws IOException in caso di errore nell'apertura della finestra
+     */
     public void registraCittadino(ActionEvent event) throws IOException {
         UniversalMethods.handleCloseButtonAction(event, btRegistraCittadino);
         UniversalMethods.vediFinestra("RegistraCittadino.fxml", "TATUM VACCINI - Registrazione cittadino");
     }
 
+    /**
+     *Questo metodo gestisce l'evento di pressione del pulsante per l'accesso del cittadino.
+     *Chiude la finestra corrente e apre la finestra per l'inserimento degli eventi avversi.
+     *Effettua anche il tentativo di login del cittadino.
+     *@param event evento di pressione del pulsante
+     *@throws IOException in caso di errore nell'apertura della finestra
+     */
     public void accessoCittadino(ActionEvent event) throws IOException{
         UniversalMethods.handleCloseButtonAction(event, btAccessoCittadino);
         UniversalMethods.vediFinestra("InserimentoEventiAvversi.fxml", "TATUM VACCINI - Inserimento eventi avversi");
@@ -28,6 +44,12 @@ public class ControllerAccessoCittadino {
         //boolean accessoConsentito = ClientCittadino.getInstance().login(); //TODO
     }
 
+    /**
+     *Questo metodo gestisce l'evento di pressione del pulsante per tornare alla homepage.
+     *Chiude la finestra corrente e apre la finestra per la homepage.
+     *@param event evento di pressione del pulsante
+     *@throws IOException in caso di errore nell'apertura della finestra
+     */
     public void tornaHomepage(ActionEvent event) throws IOException {
         UniversalMethods.handleCloseButtonAction(event, btIndietro);
         UniversalMethods.vediFinestra("Homepage.fxml", "TATUM VACCINI - Homepage");
